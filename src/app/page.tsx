2e89ca70df88c21d0d55b1d97b949a43acf97fb4
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-05-20 11:09:54 +0800

    added chat input box but not styled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ChatInput from "@/components/ChatInput";
 import Head from "next/head";
 
 export default function ChatUI() {
@@ -5,8 +6,8 @@ export default function ChatUI() {
       <div className="bg-gradient-to-r from-pink-700 to-yellow-400 h-screen">
         <div className="flex items-center justify-center h-full">
           <div className="container">
-            <main className="border-animation">
-              <h1>Pathlite.ai Chat Demo</h1>
+            <main>
+              <ChatInput />
             </main>
           </div>
         </div>
---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,6 @@
+import React, {useState, useEffect} from "react";
+
+export default function ChatInput(){
+    return <input type="text" placeholder="Type a message..."
+    />;
+}
