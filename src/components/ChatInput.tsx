5f9cf53f0e759598ed157a09ffbdfb6e67c463b2
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2024-05-20 15:03:44 +0800

    fully functional input box that can send message

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,18 +1,41 @@
+"use client"
+
 import React, {useState, useEffect} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 export default function ChatInput() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSendMessage = () => {
+        if (inputValue.trim() === "") return;
+        else {
+            console.log(inputValue);
+            setInputValue('');
+        }
+    }
+    const handleEnterDown = (event: { key: string; }) => {
+        if (event.key==="Enter") {
+            handleSendMessage();
+        }
+    }
     return (
-        <div className="chat-input-container">
-            <input 
+        <div className="chat-input-container flex items-center">
+            <div className="relative flex flex-grow">
+                <input 
                 type="text" 
                 placeholder="Ask pathlite.ai anything related to your document"
-                className="flex-grow h-10 px-3 rounded-l-lg border-2 border-gray-200 focus:outline-none focus:border-blue-500"
-            />
-            <button className="p-2 rounded-r-lg bg-blue-500 text-white">
-                <FontAwesomeIcon icon={faPaperPlane} />
-            </button>
+                className="flex-grow h-10 pl-3 pr-10 rounded-lg bg-gray-700 text-white placeholder-gray-400 focus:outline-none"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={handleEnterDown}
+                />
+                <div className="absolute right-0 top-0 h-full w-10 flex items-center justify-center rounded-r-lg">
+                    <button className="w-4 h-4 flex items-center justify-center" onClick={handleSendMessage}>
+                        <FontAwesomeIcon icon={faPaperPlane} className="text-white" />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
